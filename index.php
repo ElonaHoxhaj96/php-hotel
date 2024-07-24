8decfc13a9a9974088fc5d53fe422d9ace7ad6b2
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-24 16:28:42 +0200

    feat: added title

--- index.php
+++ index.php
@@ -48,7 +48,8 @@ $hotels = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <section class="container">      
+    <section class="container mt-5">  
+        <h1>PHP Hotel</h1>    
         <table class="table">
             <thead>
                 <tr>
