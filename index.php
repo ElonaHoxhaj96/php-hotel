169a19e3a7151f0751c7a59ec3c80efaa26d1ae8
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-24 16:26:24 +0200

    feat: printed the right value of parking. End of basic exercise

--- index.php
+++ index.php
@@ -1,45 +1,42 @@
 <?php
 
-    $hotels = [
-
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
-    // var_dump($hotels)
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
 ?>
 
 <!DOCTYPE html>
@@ -51,33 +48,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <section class="container">
-      
+    <section class="container">      
         <table class="table">
             <thead>
                 <tr>
-                    <th> Name</th>
+                    <th>Name</th>
                     <th>Descrizione</th>
                     <th>Parcheggio</th>
                     <th>Voto</th>
-                    <th>Distanza dal centro</th>
-                
+                    <th>Distanza dal centro (km)</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($hotels as $hotel_name): ?>
+                <?php foreach($hotels as $hotel): ?>
                     <tr>
-                        <th><?php echo $hotel_name['name'] ?></th>
-                        <td><?php echo $hotel_name['description'] ?></td> 
-                        <td><?php echo $hotel_name['parking'] ?></td>
-                        <td><?php echo $hotel_name['vote'] ?></td>
-                        <td><?php echo $hotel_name['distance_to_center'] ?></td>
+                        <th><?php echo $hotel['name']; ?></th>
+                        <td><?php echo $hotel['description']; ?></td> 
+                        <td><?php echo $hotel['parking'] ? 'SI' : 'NO'; ?></td>
+                        <td><?php echo $hotel['vote']; ?></td>
+                        <td><?php echo $hotel['distance_to_center']; ?></td>
                     </tr>
-                <?php endforeach?>
+                <?php endforeach; ?>
             </tbody>
-        </table>
-      
+        </table>      
     </section>
-    
 </body>
 </html>
