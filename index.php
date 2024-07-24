2868b75bb9005274a3fdc8660853e60dbc9e4899
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-24 15:57:30 +0200

    printed name of the hotel

--- index.php
+++ index.php
@@ -39,8 +39,7 @@
         ],
 
     ];
-
-var_dump($hotels)
+    // var_dump($hotels)
 ?>
 
 <!DOCTYPE html>
@@ -52,7 +51,39 @@ var_dump($hotels)
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-
+    <section class="container">
+        <div class="row">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Descrizione<th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($hotels as $hotel_name): ?>
+                    <tr>
+                    <th><?php echo $hotel_name['name'] ?></th>
+                    </tr>
+                <?php endforeach?>
+                <!-- <tr>
+                <th>2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th>3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                </tr> -->
+            </tbody>
+        </table>
+        </div>
+    </section>
     
 </body>
 </html>
